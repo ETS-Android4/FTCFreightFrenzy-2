dc6125021f31b87cbff2e04b74d230aa3f260a67
AnishJag <jagannathananish@gmail.com>
2022-01-23 17:14:22 -0600

    Adjusted VisionTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AltAutos/VisionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AltAutos/VisionTest.java
@@ -22,54 +22,10 @@ public class VisionTest extends LinearOpMode{
 
         base.init(hardwareMap, this);
 
-        base.rightDT.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        base.leftDT.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        base.rightDT.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        base.leftDT.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
         boolean keepGoing = true;
         while (keepGoing) {
             ObjectDetector.POSITIONS positionBefore = detector.getDecision();
             telemetry.addData("", positionBefore);
-        }
-
-        waitForStart();
-
-        base.gyro.resetZAxisIntegrator();
-
-        ObjectDetector.POSITIONS position = detector.getDecision();
-        detector.setTelemShow(false);
-
-        switch (position) { //hub level test result goes there <==
-            case LEFT: //lvl. 1 and open bucket
-                base.encoderDrive(0.5, 10, 10, this);
-                base.liftAuto(1, this);
-                this.telemetry.update();
-                while (base.lift.isBusy()) ;
-                base.bucket.setPosition(var.BUCKET_CLOSED);
-                base.leftClaw.setPosition(var.LCLAW_CLOSED);
-                sleep(1000);
-                break;
-            case MIDDLE: //lvl. 2 and open bucket
-                base.encoderDrive(0.5, 11.2, 11.2, this);
-                base.liftAuto(2, this);
-                this.telemetry.update();
-                while (base.lift.isBusy()) ;
-                base.bucket.setPosition(var.BUCKET_CLOSED);
-                base.leftClaw.setPosition(var.LCLAW_CLOSED);
-                sleep(1000);
-                break;
-            case RIGHT: //lvl. 3 and  open bucket
-                base.encoderDrive(0.5, 13.5, 13.5, this);
-                base.liftAuto(3, this);
-                this.telemetry.update();
-                while (base.lift.isBusy()) ;
-                base.bucket.setPosition(var.BUCKET_CLOSED);
-                base.leftClaw.setPosition(var.LCLAW_CLOSED);
-                sleep(1000);
-                break;
-
         }
     }
 }
