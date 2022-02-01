0893f39d49c091d8f8b3e922f0a777233cfe997d
AnishJag <jagannathananish@gmail.com>
2022-02-01 12:14:23 -0600

    Commented and edited BlueWHDeliverScoring.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TestAutos/BlueWHDeliverScoring.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TestAutos/BlueWHDeliverScoring.java
@@ -8,8 +8,8 @@ import org.firstinspires.ftc.teamcode.Auto.Detection.ObjectDetector;
 import org.firstinspires.ftc.teamcode.Base.MainBase;
 import org.firstinspires.ftc.teamcode.Base.Variables;
 
-//Autonomous: Delivers Pre-loaded Block, scores in
-//Position: Facing forward
+//Autonomous: Delivers Pre-loaded Block, scores additional element, parks in WH (Top-Right)
+//Position: Facing forward. Right tread lined against inside of floor-tile. Both treads touching back wall.
 
 @Autonomous(name="BLUE-WH DELIVERY & SCORE")
 public class BlueWHDeliverScoring extends LinearOpMode{
@@ -61,6 +61,8 @@ public class BlueWHDeliverScoring extends LinearOpMode{
                 base.leftClaw.setPosition(var.LCLAW_CLOSED);
                 sleep(600);
 
+                //ANYTHING BEYOND THIS POINT IS UNTESTED CODE!!!
+
                 //Drives backwards (away) from hub
                 base.encoderDrive(1.0,-10,-20,this);
 
@@ -72,7 +74,7 @@ public class BlueWHDeliverScoring extends LinearOpMode{
                 base.liftAuto(0,false,this);
 
                 //Placement before WH PARKING
-                /*base.gyroTurn(0.5,-87,this);
+                base.gyroTurn(0.5,-87,this);
                 base.encoderDrive(1.0,50,50,this); //Drives towards SHARED HUB
                 base.gyroTurn(0.5,-95,this);
 
@@ -114,25 +116,26 @@ public class BlueWHDeliverScoring extends LinearOpMode{
                 base.encoderDrive(1.0,50,50,this); //Drives towards SHARED HUB
                 base.gyroTurn(0.7,-55,this);
                 base.encoderDrive(0.7,10,10,this);
-                base.gyroTurn(0.7,-87,this);*/
+                base.gyroTurn(0.7,-87,this);
                 break;
             case MIDDLE: //SCORES IN SECOND (MIDDLE) TIER
 
                 //Positioning prior to scoring
-                base.encoderDrive(0.7, 10, 10, this); //Clears back wall
-                base.gyroTurn(0.5,30,this); //Faces shipping hub
-
+                base.encoderDrive(1.0, 5, 15,this); //Clears back wall & faces carousel
                 base.liftAuto(2,false,this);
-                base.encoderDrive(0.5,8,8,this);
-                base.bucket.setPosition(0.45);
-                sleep(400);
-                base.encoderDrive(0.3,8,7,this);
+                base.encoderDrive(1.0,7,7,this);
+
+                base.bucket.setPosition(var.BUCKET_CLOSED);
+                sleep(500);
+                base.encoderDrive(0.8,8,8,this);
 
                 base.leftClaw.setPosition(var.LCLAW_CLOSED);
                 sleep(600);
 
+                //ANYTHING BEYOND THIS POINT IS UNTESTED CODE!!!
+
                 //Drives backwards (away) from hub
-                base.encoderDrive(0.5,-15.5,-15.5,this);
+                base.encoderDrive(1.0,-10,-20,this);
 
                 //CLOSES bucket and claw
                 base.bucket.setPosition(var.BUCKET_OPEN);
@@ -142,45 +145,54 @@ public class BlueWHDeliverScoring extends LinearOpMode{
                 base.liftAuto(0,false,this);
 
                 //Placement before WH PARKING
-                base.gyroTurn(0.5,-65,this); //Turns towards SHARED HUB
-                base.encoderDrive(0.8,55,55,this); //Drives towards SHARED HUB
-                base.gyroTurn(0.5,0,this);
-                base.encoderDrive(0.5,5,5,this);
                 base.gyroTurn(0.5,-87,this);
+                base.encoderDrive(1.0,50,50,this); //Drives towards SHARED HUB
+                base.gyroTurn(0.5,-95,this);
+
+                //OPENS bucket and claw
+                base.bucket.setPosition(var.BUCKET_CLOSED);
+                base.leftClaw.setPosition(var.LCLAW_CLOSED);
+
+                base.encoderDrive(0.3,10,10,this);
+
+                //CLOSES bucket and claw
+                base.leftClaw.setPosition(var.LCLAW_OPEN);
+                sleep(300);
+                base.bucket.setPosition(var.BUCKET_OPEN);
+
+                base.gyroTurn(0.7,87,this);
+                base.encoderDrive(1.0,30,30,this); //Drives towards HUB
+                base.gyroTurn(0.5,30,this);
+
+                base.liftAuto(1,false,this);
+                base.encoderDrive(0.6,10,10,this);
+                base.bucket.setPosition(var.BUCKET_CLOSED);
+                sleep(400);
+                base.encoderDrive(0.5,5,5,this);
+                base.leftClaw.setPosition(var.LCLAW_CLOSED);
+                sleep(600);
+
+                //Drives backwards (away) from hub
+                base.encoderDrive(1.0,-14,-14,this);
+
+                //CLOSES bucket and claw
+                base.bucket.setPosition(var.BUCKET_OPEN);
+                base.leftClaw.setPosition(var.LCLAW_OPEN);
+
+                //Brings down lift while parking
+                base.liftAuto(0,false,this);
+
+                //Placement before WH PARKING
+                base.gyroTurn(0.7,-69,this); //Turns towards SHARED HUB
+                base.encoderDrive(1.0,50,50,this); //Drives towards SHARED HUB
+                base.gyroTurn(0.7,-55,this);
+                base.encoderDrive(0.7,10,10,this);
+                base.gyroTurn(0.7,-87,this);
                 break;
             case RIGHT: //SCORES IN THIRD (TOP) TIER
 
-                //Positioning prior to scoring
-                base.encoderDrive(0.7, 10, 10, this); //Clears back wall
-                base.gyroTurn(0.5,90,this); //Faces shipping hub
-                base.encoderDrive(0.7,20,20,this);
-                base.gyroTurn(0.5,0,this);
+                //REQUIRES REWRITING (Possible task for Immanuel or Claire)
 
-                base.liftAuto(3,false,this);
-                base.encoderDrive(0.2,11,11,this);
-                base.bucket.setPosition(var.BUCKET_CLOSED);
-                sleep(500);
-
-                base.leftClaw.setPosition(var.LCLAW_CLOSED);
-                sleep(600);
-
-                //Drives backwards (away) from hub
-                base.encoderDrive(0.5,-10,-10,this);
-
-                //CLOSES bucket and claw
-                base.bucket.setPosition(var.BUCKET_OPEN);
-                base.leftClaw.setPosition(var.LCLAW_OPEN);
-
-                //Brings down lift while parking
-                base.liftAuto(0,false,this);
-
-                //Placement before WH PARKING
-                base.gyroTurn(0.5,-87,this); //Turns towards SHARED HUB
-                base.encoderDrive(1.0,60,60,this); //Drives towards SHARED HUB
-                base.gyroTurn(0.5,0,this);
-                base.encoderDrive(0.5,17,17,this);
-                base.gyroTurn(0.5,-90,this);
-                base.encoderDrive(0.6,10,10,this);
                 break;
         }
     }
