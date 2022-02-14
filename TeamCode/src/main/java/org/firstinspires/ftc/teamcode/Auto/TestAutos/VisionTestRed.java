53c65c1732904f4803a9a45faef65264a40736d4
AnishJag <jagannathananish@gmail.com>
2022-02-13 20:06:28 -0600

    Fixed VisionTest to adjust for while loop.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TestAutos/VisionTestRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TestAutos/VisionTestRed.java
@@ -20,36 +20,37 @@ public class VisionTestRed extends LinearOpMode{
 
         ObjectDetector detector = new ObjectDetector(this, true,true);
 
-        if (gamepad1.dpad_up && !GP1_DPADUP_HELD) {
-            GP1_DPADUP_HELD = true;
-            detector = new ObjectDetector(this,true,false);
-        }
-        else if ((gamepad1.dpad_left || gamepad1.dpad_right) && !GP1_DPADSIDE_HELD) {
-            GP1_DPADSIDE_HELD = true;
-            detector = new ObjectDetector(this,false,false);
-        }
-        else if (gamepad1.y && !GP1_Y_HELD) {
-            GP1_Y_HELD = true;
-            detector = new ObjectDetector(this,true,true);
-        }
-        else {
-            detector = new ObjectDetector(this,false,true);
-        }
-
-        if (!gamepad1.dpad_up) {
-            GP1_DPADUP_HELD = false;
-        }
-        if (!gamepad1.dpad_left || !gamepad1.dpad_right) {
-            GP1_DPADSIDE_HELD = false;
-        }
-        if (!gamepad1.y) {
-            GP1_Y_HELD = false;
-        }
-
         base.init(hardwareMap, this);
 
         //Once Gamepad 1 'A' is pressed, code exits while loop and provides safe stoppage of VISION Test.
         while (!gamepad1.a) {
+
+            if (gamepad1.dpad_up && !GP1_DPADUP_HELD) {
+                GP1_DPADUP_HELD = true;
+                detector = new ObjectDetector(this,true,false);
+            }
+            else if ((gamepad1.dpad_left || gamepad1.dpad_right) && !GP1_DPADSIDE_HELD) {
+                GP1_DPADSIDE_HELD = true;
+                detector = new ObjectDetector(this,false,false);
+            }
+            else if (gamepad1.y && !GP1_Y_HELD) {
+                GP1_Y_HELD = true;
+                detector = new ObjectDetector(this,true,true);
+            }
+            else {
+                detector = new ObjectDetector(this,false,true);
+            }
+
+            if (!gamepad1.dpad_up) {
+                GP1_DPADUP_HELD = false;
+            }
+            if (!gamepad1.dpad_left || !gamepad1.dpad_right) {
+                GP1_DPADSIDE_HELD = false;
+            }
+            if (!gamepad1.y) {
+                GP1_Y_HELD = false;
+            }
+
             ObjectDetector.POSITIONS positionBefore = detector.getDecision();
             telemetry.addData("", positionBefore);
         }
