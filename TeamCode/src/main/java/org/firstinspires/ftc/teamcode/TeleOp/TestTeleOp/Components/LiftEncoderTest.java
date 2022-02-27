83a04c25f60ab2f5d699b053e9cfbb462f444a40
AnishJag <jagannathananish@gmail.com>
2022-02-26 22:48:17 -0600

    Moved LiftTest to different package.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestTeleOp/Components/LiftEncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestTeleOp/Components/LiftEncoderTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.TeleOp.TestTeleOp;
+package org.firstinspires.ftc.teamcode.TeleOp.TestTeleOp.Components;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
